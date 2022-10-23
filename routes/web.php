6c73db0614f2d00469f34b8794cc520b1523683f
Ameen1sn <ameensnad@gmail.com>
2022-10-23 22:54:29 +0300

    a0commit

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/a', function () {
-    return view('welcome');
-});
 Route::get('/', function () {
-    return 'ameen';
+    return view('edraak');
+});
+Route::get('/w', function () {
+    return 'welcome';
 });
